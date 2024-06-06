e0f364efd1bc974bfc7b4a1fbc9494fbc6dc588d
UladzimirIvanov <ivanouum@gmail.com>
2024-06-06 16:37:47 +0300

    Accounts File Reader finished

---
+++ src/AccontsReader.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class AccontsReader {
+    public void watchDataAccounts(){
+        File file = new File("src\\Files\\Accounts.txt");
+        System.out.println(file.exists());
+
+        try (FileReader stream = new FileReader("src\\Files\\Accounts.txt")){
+            int i;
+            while ((i = stream.read()) != -1){
+                System.out.print((char)i);
+            }
+        } catch (IOException e){
+            System.out.println(e);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Main {
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1. Вызов операции парсинга файлов перевода из input");
+        System.out.println("2. Вызов операции вывода списка всех переводов из файла-отчёта");
+        System.out.println("3. Информация по счетам");
+
+        int PARS_FILE = 1;
+        int REPORT_FILE = 2;
+        int ACCOUNTS_FILE = 3;
+
+        int userInput;
+
+        while (true) {
+            try {
+                System.out.println("Введите цифру от 1 до 2:");
+                userInput = new Scanner(System.in).nextInt();
+                if (userInput >= 1 && userInput <= 3) {
+                    break;
+                } else {
+                    System.out.println("Некорректные данные");
+                    System.out.println();
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Некорректные данные");
+                System.out.println();
+            }
+        }
+
+        if (userInput == PARS_FILE) {
+            System.out.println("Пока не работает");
+        }
+        if (userInput == REPORT_FILE) {
+            System.out.println("Пока не работает");
+        }
+        if (userInput == ACCOUNTS_FILE) {
+            AccontsReader accontsReader = new AccontsReader();
+            accontsReader.watchDataAccounts();
+        }
+
+    }
 }
