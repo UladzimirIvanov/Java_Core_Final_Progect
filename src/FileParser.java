4b4b1edc7836fe7a6ed18735d344c41afe3ac878
UladzimirIvanov <ivanouum@gmail.com>
2024-06-06 17:04:34 +0300

    FileParser 30

--- src/FileParser.java
+++ src/FileParser.java
@@ -1,2 +1,15 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FileParser {
+    public void test(){
+        Pattern p = Pattern.compile("[0-9]{5}-[0-9]{5}");
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append("End");
+        System.out.println(sb);
+        Matcher m = p.matcher("Hello i'm Daniel!"); // Передаём строку паттерна в матчер
+        while (m.find()){ //.find() перегружен, можно оставить пустым или указать с какого символа искать
+            System.out.println(m.group());
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1. Вызов операции парсинга файлов перевода из input");
+        /*System.out.println("1. Вызов операции парсинга файлов перевода из input");
         System.out.println("2. Вызов операции вывода списка всех переводов из файла-отчёта");
         System.out.println("3. Информация по счетам");
 
@@ -41,7 +44,35 @@ public class Main {
         if (userInput == ACCOUNTS_FILE) {
             AccontsReader accontsReader = new AccontsReader();
             accontsReader.watchDataAccounts();
+        }*/
+
+
+
+
+
+        StringBuilder sb = new StringBuilder();
+        ArrayList<String> list = new ArrayList<>();
+
+        try (FileReader stream = new FileReader("src\\Files\\Accounts.txt")){
+            int i;
+            while ((i = stream.read()) != -1){
+                sb.append((char)i);
+            }
+        } catch (IOException e){
+            System.out.println(e);
         }
 
+        System.out.println(sb);
+
+        System.out.println();
+
+        Pattern p = Pattern.compile("[0-9]{5}-[0-9]{5}");
+        Matcher m = p.matcher(sb);
+        while (m.find()){
+            list.add(m.group());
+        }
+
+        System.out.println();
+        System.out.println(list);
     }
 }
